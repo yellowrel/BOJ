a5ff1c5ef067890d67a68c278fa9fda5c8e1ad32
yellowrel <ghkdtmdwns12@gmail.com>
2023-06-16 17:05:43 +0900

    230601 boj study

---
+++ BOJ_1717.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1717 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+    }
+}
